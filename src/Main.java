d77830488c95f013b56ece22c067daaac36e8434
MinQ <2024513@daelim.ac.kr>
2024-11-20 19:27:46 +0900

    fixture-2 개발 했습니다. 어려워요 git

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
         //이부분 변경 된거에요.....
         //이분 풀 안되어있는데?????
+
+        //fixture-2 개발 많이 했습니다. !~~~~
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
