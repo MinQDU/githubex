0899149a40758269528926c05f3dbf72de48dbcc
MinQ <2024513@daelim.ac.kr>
2024-11-20 18:44:02 +0900

    이부분 풀 테스트용

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,8 @@ public class Main {
 
         //이부분 변경 된거에요.....
         //이분 풀 안되어있는데?????
-        //이거 풀하기위해서 적어보겠습니다.
+        //안ㅇ머ㅏㅣ
+
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
