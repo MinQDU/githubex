bcb0f21f965ae9943d19be6ed02b13333fa5bbd3
MinQ <2024513@daelim.ac.kr>
2024-11-20 19:09:16 +0900

    fixture-1 branch 생성

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        //fixture-1
 
         //이부분 변경 된거에요.....
         //이분 풀 안되어있는데?????
